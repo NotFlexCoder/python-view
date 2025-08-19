466c7aa28fcd19195ff578c53dd0895ab669603e
Flex Coder <jaychonkar617@gmail.com>
2025-08-19 08:12:27 +0530

    index.py

--- index.py
+++ index.py
@@ -3,11 +3,15 @@ import io, contextlib
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def run_code():
     code = request.args.get("code")
+    if request.method == "POST" and not code:
+        code = request.data.decode("utf-8")
+
     if not code:
         return Response("No code provided", mimetype="text/plain", status=400)
+
     try:
         buffer = io.StringIO()
         with contextlib.redirect_stdout(buffer):
